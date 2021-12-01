8f09ba9cce6c87a9d43ea9a9f0d71a9b7736e457
Joshua Moir <j.moir.nz@gmail.com>
2021-12-01 14:01:23 +0100

    this is the intial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// console.log(process.argv);
+const { exec, execSync } = require("child_process");
+// const { stdout, stderr } = require("process");
+const [message, branchname] = process.argv.slice(2);
+if (message && branchname) {
+  let { err, stdout, stderr } = execSync("git add .");
+  if (!err) {
+    let { error, stdout, stderr } = execSync(`git commit -m "${message}"`);
+
+    if (!error) {
+      let { errormessage, stdout, stderr } = execSync(
+        `git push -u origin "${branchname}"`
+      );
+      if (!errormessage) {
+        console.log("successfully pushed your code to github");
+        console.log(stdout);
+      }
+    }
+  }
+}
+
+//   exec("git add .", (err, stdout, stderr) => {
+//     if (err) {
+//       throw new Error("There is a problem");
+//     }
+//     exec(`git commit -m "${message}"`, (err, stdout, stderr) => {
+//       if (err) {
+//         throw new Error("There is a problem");
+//       }
+//       exec(`git push origin ${branchname}`, (err, stdout, stderr) => {
+//         if (err) {
+//           throw new Error("There is a problem");
+//         }
+//         console.log("Successfully pushed to Github ");
+//       });
+//     });
+//   });
+// } else {
+//   console.log(
+//     "Please Provide Two Arguments ==> 1 - Your Commit Message, 2 - Your Branch Name "
+//   );
